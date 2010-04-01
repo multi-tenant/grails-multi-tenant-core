c28d8ab544cd904983a96c94075616484664a2df
scryan <scryan@832c1c66-4827-0410-8465-ccb17913cd09>
2010-04-01 21:23:36 +0000

    This will update the datasource loader and domain to tenant map loading to allow for a centralized configuration database.  This forces the datasource to tenant mapping to be loaded from the default datasource ala tenant 0 so that we avoid a circular dependency.
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-multi-tenant/trunk@60740 832c1c66-4827-0410-8465-ccb17913cd09

--- src/groovy/com/infusion/tenant/DomainNameDatabaseTenantResolver.groovy
+++ src/groovy/com/infusion/tenant/DomainNameDatabaseTenantResolver.groovy
@@ -1,54 +1,84 @@
 package com.infusion.tenant;
 
-
-import com.infusion.util.event.EventBroker
 import com.infusion.util.event.groovy.GroovyEventBroker
 import com.infusion.util.domain.event.HibernateEvent
+import com.infusion.tenant.util.TenantUtils
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
 import org.apache.log4j.Logger
 
 /**
- * Implementation that looks up tenantIds from a local table DomainTenantMap that stores domain name to tenantId mappings. 
+ * Implementation that looks up tenantIds from a local table DomainTenantMap that stores domain name to tenantId mappings
+ * in support of the single or multi tenant mode.  The data will either be looked up from a configuration database or from the default
+ * datasource as defined in the datasource config file.  It is not a good idea to store this data in every client database.
  */
-public class DomainNameDatabaseTenantResolver extends BaseDomainNameTenantResolver implements ApplicationContextAware {
+public class DomainNameDatabaseTenantResolver extends BaseDomainNameTenantResolver implements ApplicationContextAware
+{
+    /** This is a logger for logging status and error messages.           */
+    private static Logger log = Logger.getLogger(getClass());
 
-  private static Logger log = Logger.getLogger(getClass());
-  
-  /**
-   * Used for listening to save events for DomainTenantMap domain class
-   */
-  GroovyEventBroker eventBroker
-  ApplicationContext applicationContext
+    /**
+     * Used for listening to save events for DomainTenantMap domain class
+     */
+    GroovyEventBroker eventBroker
+    /**
+     * This is the injected context that is used to look up the DomainTenantMap Bean.
+     */
+    ApplicationContext applicationContext
+    /**
+     * This will initialize the data for the application by loading the domain name tenant map data.  This data is loaded
+     * from tenant 0 which could either be a separate configuration database or the default datasource as defined
+     * in the datasources config file.  This insures we don't end up with cyclic dependencies and that we don't have to store
+     * the configuration data with each client.
+     */
+    void initialize()
+    {
 
-
-  public DomainNameDatabaseTenantResolver() {
-  }
-
-  /**
-   * The event broker listens for every time a record is saved, then calls a refresh on the
-   * list of hosts.
-   */
-  public void setEventBroker(GroovyEventBroker eventBroker) {
-    if (eventBroker != null) {
-      eventBroker.subscribe("hibernate.${HibernateEvent.saveOrUpdate}.DomainTenantMap") {
-        event, broker ->
-        log.info "DomainTenantMap was saved, reloading resolver"
-        this.initialize();
-      }
+        // If this is single tenant per database then we need to hard code to tenant 0 and use a configuration
+        // database or the default datasouce.
+        if (ConfigurationHolder.config.tenant.mode == "singleTenant" &&
+                ConfigurationHolder.config.tenant.configDB == true)
+        {
+            log.info "Initializing Domain Name Map for Single Tenant with Config DB support"
+            TenantUtils tenantUtils = applicationContext.getBean("tenantUtils")
+            tenantUtils.doWithTenant(0) {
+                loadDomainTenantMap()
+            }
+        }
+        else
+        {
+            log.info "Initializing Domain Name Map for Multi Tenant support or single tenant without config DB support"
+            loadDomainTenantMap()
+        }
     }
-  }
-
-  public void initialize() {
-
-    hosts.clear()
-    log.info "Reloading resolver"
-    //This will load all domain tenants, regardless of which tenant they're for
-    def list = applicationContext.getBean("tenant.DomainTenantMap").findAll("from tenant.DomainTenantMap");
-    list.each {map ->
-      log.debug "Domain->Tenant: ${map.domainName}->${map.mappedTenantId}"
-      hosts.put(map.domainName, map.mappedTenantId)
+    /**
+     * This will load the hosts data which contains the map between domain and tenant.
+     */
+    void loadDomainTenantMap()
+    {
+        hosts.clear()
+        log.info "Loading Domain Name to Tenant information from the database object DomainTenantMap"
+        // Load the tenant information for the domain to tenant id from the database.
+        def list = applicationContext.getBean("tenant.DomainTenantMap").findAll("from tenant.DomainTenantMap");
+        list.each {map ->
+            if (log.isDebugEnabled()) log.debug "Domain->Tenant: ${map.domainName}->${map.mappedTenantId}"
+            hosts.put(map.domainName, map.mappedTenantId)
+        }
     }
-  }
-
+    /**
+     * The event broker listens for every time a record is saved, then calls a refresh on the
+     * list of hosts whenever the DomainTenantMap object is changed via a save or update.
+     */
+    void setEventBroker(GroovyEventBroker inEventBroker)
+    {
+        if (inEventBroker != null)
+        {
+            inEventBroker.subscribe("hibernate.${HibernateEvent.saveOrUpdate}.DomainTenantMap") {
+                event, broker ->
+                log.info "DomainTenantMap was changed via a save or update.  Reloading the Domain to Tenant information from the database."
+                this.initialize();
+            }
+        }
+    }
 }
--- src/java/com/infusion/tenant/datasource/TenantJndiDataSource.java
+++ src/java/com/infusion/tenant/datasource/TenantJndiDataSource.java
@@ -1,34 +1,78 @@
 package com.infusion.tenant.datasource;
 
+import org.apache.log4j.Logger;
 import org.springframework.jndi.JndiObjectFactoryBean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.mock.jndi.SimpleNamingContextBuilder;
 import com.infusion.tenant.CurrentTenant;
 
 import javax.naming.NamingException;
 
-/**
- * This class allows you to have a switching jndi datasource per tenant with the multi-tenant plugin
- */
-public class TenantJndiDataSource extends JndiObjectFactoryBean {
+/** This class allows you to have a switching jndi datasource per tenant with the multi-tenant plugin */
+public class TenantJndiDataSource extends JndiObjectFactoryBean
+{
+    /** This is the resolver to be used to determine the datasource. */
     private DataSourceUrlResolver dataSourceUrlResolver;
+    /** This is the tenant this data source is associated with. */
     private CurrentTenant currentTenant;
-    public static final boolean multiTenant = true;
+    /**
+     * This allows the TenantBeanFactoryPostProcessor to assign a proxy to this object in spring since there is one of
+     * there for each client.
+     */
+    public static final boolean multiTenant=true;
+    /** This is a logger for logging status or errors */
+    private static Logger log=Logger.getLogger(TenantJndiDataSource.class);
 
+    /**
+     * This method will look up the datasoure URL for the given tenant and return that url.
+     *
+     * @return The data source URL for the tenant via the resolver.
+     */
     @Override
-    public String getJndiName() {
-        final String jndiNameForTenant = dataSourceUrlResolver.getDataSourceUrl(currentTenant.get());
-        if (jndiNameForTenant != null) {
-            super.setJndiName(jndiNameForTenant);
+    public String getJndiName()
+    {
+        if (log.isDebugEnabled())
+            log.debug("Resolving JNDI datasource for tenant " + currentTenant.get() + " and resolver " + dataSourceUrlResolver);
+        // If we are using tenent 0 then return the default datasource
+        // TODO Can we handle a hard coded config datasource?
+        if (currentTenant.get() == 0)
+        {
+            final String jndiNameForTenant=super.getJndiName();
+        } else
+        {
+            // If not tenant 0 then look up the jndi name from the tenant.
+            final String jndiNameForTenant=dataSourceUrlResolver.getDataSourceUrl(currentTenant.get());
+            // If we found a jndi name we store it in the datasource otherwise we determine if it is an error or not.
+            if (jndiNameForTenant != null)
+            {
+                super.setJndiName(jndiNameForTenant);
+            } else
+            {
+                // TODO If this is single tenant we want to throw an exception to warn of a possible issue?
+            }
         }
+        if (log.isDebugEnabled())
+            log.debug("Located JNDI datasource " + super.getJndiName() + " for tenant " + currentTenant.get());
         return super.getJndiName();
     }
 
-    public void setDataSourceUrlResolver(DataSourceUrlResolver dataSourceUrlResolver) {
-        this.dataSourceUrlResolver = dataSourceUrlResolver;
+    /**
+     * This will set the resolver type for this datasource object.
+     *
+     * @param inDataSourceUrlResolver The resolver to use to determine the datasource for the tenant.  The types could
+     *                                be database or properties file.
+     */
+
+    public void setDataSourceUrlResolver(DataSourceUrlResolver inDataSourceUrlResolver)
+    {
+        this.dataSourceUrlResolver=inDataSourceUrlResolver;
     }
 
-    public void setCurrentTenant(CurrentTenant currentTenant) {
-        this.currentTenant = currentTenant;
+    /**
+     * Since this object is cached this allows you to resolve which tenant this resource applies to.
+     *
+     * @param inCurrentTenant The tenant id this data source is associated with.
+     */
+    public void setCurrentTenant(CurrentTenant inCurrentTenant)
+    {
+        this.currentTenant=inCurrentTenant;
     }
 }
