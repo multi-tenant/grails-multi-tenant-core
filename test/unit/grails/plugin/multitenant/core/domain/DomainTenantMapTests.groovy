7870a4159ebe66c87a4361adea5f2d3ab77e5695
Scott Ryan <scott@theryansplace.com>
2010-10-02 10:37:06 -0600

    Add test directory

---
+++ test/unit/grails/plugin/multitenant/core/domain/DomainTenantMapTests.groovy
@@ -0,0 +1,17 @@
+package grails.plugin.multitenant.core.domain
+
+import grails.test.*
+
+class DomainTenantMapTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
