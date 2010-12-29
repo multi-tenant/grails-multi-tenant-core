d56e1b468809a8e27c39e052e4809a0f1758903d
sryan <scottryan@tca-e259b880b06.triplecreek.local>
2010-12-29 13:06:36 -0700

    fix debugging to log the jndi data source used for the tenant.  Also added some documentation.

--- src/java/grails/plugin/multitenant/core/datasource/TenantTransactionAwareDataSourceProxy.java
+++ src/java/grails/plugin/multitenant/core/datasource/TenantTransactionAwareDataSourceProxy.java
@@ -9,24 +9,25 @@ import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 /**
- * Created by IntelliJ IDEA.
- * User: scottryan
- * Date: Aug 9, 2010
- * Time: 2:10:14 PM
- * To change this template use File | Settings | File Templates.
+ * This class will intercept any calls by the grails infrastructure for a data source and return the proper
+ * data source for the current tenant.  If a transaction is already open and you make a tenant change the
+ * data source will not change.  If you want to change tenants then you need to close all open transactions and then
+ * change tenant and open the transaction.  Also you should flush and clear the hibernate session first level cache so
+ * you don't mix tenant data in the cache.  This class only supports JNDI based data sources at the current time.
  */
-public class TenantTransactionAwareDataSourceProxy extends TransactionAwareDataSourceProxy {
+public class TenantTransactionAwareDataSourceProxy extends TransactionAwareDataSourceProxy
+{
     /**
-     * This is the resolver to be used to determine the datasource.
+     * This is the data source url resolver to be used to determine the data source url.
      */
     private DataSourceUrlResolver dataSourceUrlResolver;
     /**
-     * This is the tenant this data source is associated with.
+     * This is the thread local tenant that is active at the time of the interation with the class.
      */
     private CurrentTenant currentTenant;
     /**
      * This allows the TenantBeanFactoryPostProcessor to assign a proxy to this object in spring since there is one of
-     * there for each client.
+     * these for each client.
      */
     public static final boolean multiTenant = true;
     /**
@@ -34,40 +35,57 @@ public class TenantTransactionAwareDataSourceProxy extends TransactionAwareDataS
      */
     private static Logger log = Logger.getLogger(TenantTransactionAwareDataSourceProxy.class);
 
-    TenantTransactionAwareDataSourceProxy(DataSource targetDataSource) {
+    /**
+     * This is the default constructor.
+     *
+     * @param targetDataSource The target data source for this class.
+     */
+    TenantTransactionAwareDataSourceProxy(DataSource targetDataSource)
+    {
         super(targetDataSource);
     }
 
+    /**
+     * This method will return the actual data source for the active tenant.  If no tenant can be resolve then the
+     * default data source is returned and assumed to connect to the configuration database as tenant 0.
+     *
+     * @return The data source for the current thread local tenant.
+     */
     @Override
-    public DataSource getTargetDataSource() {
+    public DataSource getTargetDataSource()
+    {
         DataSource ds = super.getTargetDataSource();
-        if (currentTenant.get() != 0) {
+        if (currentTenant.get() != 0)
+        {
             // If not tenant 0 then look up the jndi name from the tenant.
             final String jndiNameForTenant = dataSourceUrlResolver.getDataSourceUrl(currentTenant.get());
-            try {
+            if (log.isDebugEnabled())
+            {
+                log.debug("Returning the jndi dataSource " + jndiNameForTenant + " for tenant " + currentTenant.get());
+            }
+            try
+            {
                 // TODO Support other than JNDI Data Sources
                 // TODO Either cache the context lookup or cache the data source for performance
                 Context ctx = new InitialContext();
                 ds = (DataSource) ctx.lookup(jndiNameForTenant);
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 log.fatal("Exception in Multi-tenant data source provider", ex);
             }
-        }
-        if (log.isDebugEnabled()) {
-            log.debug("Returning the dataSource " + ds.toString() + " for tenant " + currentTenant.get());
         }
         return ds;
     }
 
     /**
-     * This will set the resolver type for this datasource object.
+     * This will set the resolver type for this data source object.
      *
-     * @param inDataSourceUrlResolver The resolver to use to determine the datasource for the tenant.  The types could
+     * @param inDataSourceUrlResolver The resolver to use to determine the data source for the tenant.  The types could
      *                                be database or properties file.
      */
-
-    public void setDataSourceUrlResolver(DataSourceUrlResolver inDataSourceUrlResolver) {
+    public void setDataSourceUrlResolver(DataSourceUrlResolver inDataSourceUrlResolver)
+    {
         this.dataSourceUrlResolver = inDataSourceUrlResolver;
     }
 
@@ -76,7 +94,8 @@ public class TenantTransactionAwareDataSourceProxy extends TransactionAwareDataS
      *
      * @param inCurrentTenant The tenant id this data source is associated with.
      */
-    public void setCurrentTenant(CurrentTenant inCurrentTenant) {
+    public void setCurrentTenant(CurrentTenant inCurrentTenant)
+    {
         this.currentTenant = inCurrentTenant;
     }
 }
