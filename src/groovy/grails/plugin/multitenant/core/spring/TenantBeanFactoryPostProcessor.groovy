92a27914aa9325f946bd9879c26e705dbf8950a1
lucastex <lucastex@gmail.com>
2011-02-09 21:51:09 -0200

    TenantBeanFactoryPostProcessor code cleaned

--- src/groovy/grails/plugin/multitenant/core/spring/TenantBeanFactoryPostProcessor.groovy
+++ src/groovy/grails/plugin/multitenant/core/spring/TenantBeanFactoryPostProcessor.groovy
@@ -1,89 +1,92 @@
-package grails.plugin.multitenant.core.spring;
+package grails.plugin.multitenant.core.spring
 
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.RuntimeBeanReference;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
-import org.springframework.beans.BeansException;
+import org.apache.log4j.Logger
 
-
-import org.springframework.aop.framework.ProxyFactoryBean;
-import org.springframework.core.Ordered;
-import org.codehaus.groovy.grails.commons.spring.RuntimeSpringConfiguration;
-import org.codehaus.groovy.grails.commons.spring.DefaultRuntimeSpringConfiguration;
-import org.codehaus.groovy.grails.commons.spring.BeanConfiguration;
-import org.codehaus.groovy.grails.commons.GrailsClassUtils;
-import org.apache.log4j.Logger;
 import com.infusion.util.CollectionUtil
-import org.codehaus.groovy.grails.commons.ConfigurationHolder;
+
+import org.springframework.core.Ordered
+import org.springframework.beans.BeansException
+import org.springframework.aop.framework.ProxyFactoryBean
+import org.springframework.beans.factory.config.BeanDefinition
+import org.springframework.beans.factory.config.RuntimeBeanReference
+import org.springframework.beans.factory.support.GenericBeanDefinition
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor
+import org.springframework.beans.factory.support.DefaultListableBeanFactory
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
+
+import org.codehaus.groovy.grails.commons.GrailsClassUtils
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+import org.codehaus.groovy.grails.commons.spring.BeanConfiguration
+import org.codehaus.groovy.grails.commons.spring.RuntimeSpringConfiguration
+import org.codehaus.groovy.grails.commons.spring.DefaultRuntimeSpringConfiguration
 
 /**
  * Examines all spring beans after all definitions have been loaded by the container, and converts
- * multiTenant beans to use the spring aop proxies.  This code executes before ANY beans are initialized.
+ * multiTenant beans to use the spring aop proxies. This code executes before ANY beans are initialized.
  */
 public class TenantBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
 
-  Logger log = Logger.getLogger(TenantBeanFactoryPostProcessor.class.getName());
+    Logger log = Logger.getLogger(TenantBeanFactoryPostProcessor.class.getName())
 
-  public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
 
-    //This cast exposes you bean registration capabilities
-    DefaultListableBeanFactory applicationContext = (DefaultListableBeanFactory) beanFactory;
+        //This cast exposes you bean registration capabilities
+        DefaultListableBeanFactory applicationContext = (DefaultListableBeanFactory) beanFactory
 
-    //Locate and proxy multi-tenant beans
-    RuntimeSpringConfiguration springConfig = new DefaultRuntimeSpringConfiguration();
+        //Locate and proxy multi-tenant beans
+        RuntimeSpringConfiguration springConfig = new DefaultRuntimeSpringConfiguration()
 
-    List uniquePerTenantBeans = ConfigurationHolder.config.tenant.uniquePerTenantBeans ?: [];
+        List uniquePerTenantBeans = ConfigurationHolder.config.tenant.uniquePerTenantBeans ?: []
 
-    for (String beanName: applicationContext.getBeanDefinitionNames()) {
-      BeanDefinition oldBeanDef = applicationContext.getBeanDefinition(beanName);
+        for (String beanName: applicationContext.getBeanDefinitionNames()) {
+	
+            BeanDefinition oldBeanDef = applicationContext.getBeanDefinition(beanName)
 
-      if (oldBeanDef.getBeanClassName() == null) {
-        continue;
-      }
-      //Attempt to load the class so I can tell whether or not to proxy this bean.  Load
-      //the class because we need to examine a static property.
-      Class clazz = null;
-      try {
-        clazz = TenantBeanFactoryPostProcessor.class.getClassLoader().loadClass(oldBeanDef.getBeanClassName());
-      } catch (Exception e) {
-        log.error("Unabled to load class for " + oldBeanDef.getBeanClassName());
-        continue;
-        //throw new FatalBeanException("Error running postprocessor", e);
-      }
+            if (oldBeanDef.getBeanClassName() == null) {
+                continue
+            }
 
+            //Attempt to load the class so I can tell whether or not to proxy this bean.  Load
+            //the class because we need to examine a static property.
+            Class clazz = null
+            try {
+                clazz = TenantBeanFactoryPostProcessor.class.getClassLoader().loadClass(oldBeanDef.getBeanClassName())
+            } catch (Exception e) {
+                log.error("Unabled to load class for " + oldBeanDef.getBeanClassName())
+                continue
+                //throw new FatalBeanException("Error running postprocessor", e)
+            }
 
-      boolean staticMultiTenantSet = (Boolean) GrailsClassUtils.getStaticPropertyValue(clazz, "multiTenant") ?: false;
-      boolean staticUniquePerTenantSet = (Boolean) GrailsClassUtils.getStaticPropertyValue(clazz, "uniquePerTenant") ?: false;
-      if (staticMultiTenantSet || staticUniquePerTenantSet || uniquePerTenantBeans.contains(beanName)) {
+            boolean staticMultiTenantSet = (Boolean) GrailsClassUtils.getStaticPropertyValue(clazz, "multiTenant") ?: false
+            boolean staticUniquePerTenantSet = (Boolean) GrailsClassUtils.getStaticPropertyValue(clazz, "uniquePerTenant") ?: false
 
-        //Convert the original bean definition from singleton to prototype (because we'll
-        //need to create one bean for each tenant)
-        BeanDefinition prototypeBeanDefinition = new GenericBeanDefinition(oldBeanDef);
-        prototypeBeanDefinition.setScope(BeanDefinition.SCOPE_PROTOTYPE);
-        applicationContext.registerBeanDefinition("__" + beanName, prototypeBeanDefinition);
+            if (staticMultiTenantSet || staticUniquePerTenantSet || uniquePerTenantBeans.contains(beanName)) {
 
-        //Just moves the original bean definition to a new location
-        applicationContext.registerBeanDefinition("_" + beanName, oldBeanDef);
+                //Convert the original bean definition from singleton to prototype (because we'll
+                //need to create one bean for each tenant)
+                BeanDefinition prototypeBeanDefinition = new GenericBeanDefinition(oldBeanDef)
+                prototypeBeanDefinition.setScope(BeanDefinition.SCOPE_PROTOTYPE)
+                applicationContext.registerBeanDefinition("__" + beanName, prototypeBeanDefinition)
 
-        //Create advice for this bean
-        BeanConfiguration adviceBean = springConfig.createSingletonBean(TenantMethodInterceptor.class);
-        adviceBean.addProperty("oldBeanName", "__" + beanName);
-        adviceBean.addProperty("tenantBeanContainer", new RuntimeBeanReference("tenantBeanContainer"));
-        applicationContext.registerBeanDefinition(beanName + "_advice", adviceBean.getBeanDefinition());
+                //Just moves the original bean definition to a new location
+                applicationContext.registerBeanDefinition("_" + beanName, oldBeanDef)
 
-        //Create the actual proxied bean, with the same name as the original bean
-        BeanConfiguration proxiedBean = springConfig.createSingletonBean(ProxyFactoryBean.class);
-        proxiedBean.addProperty("target", new RuntimeBeanReference("_" + beanName));
-        proxiedBean.addProperty("interceptorNames", CollectionUtil.QuickList(beanName + "_advice"));
-        applicationContext.registerBeanDefinition(beanName, proxiedBean.getBeanDefinition());
-      }
+                //Create advice for this bean
+                BeanConfiguration adviceBean = springConfig.createSingletonBean(TenantMethodInterceptor.class)
+                adviceBean.addProperty("oldBeanName", "__" + beanName)
+                adviceBean.addProperty("tenantBeanContainer", new RuntimeBeanReference("tenantBeanContainer"))
+                applicationContext.registerBeanDefinition(beanName + "_advice", adviceBean.getBeanDefinition())
+
+                //Create the actual proxied bean, with the same name as the original bean
+                BeanConfiguration proxiedBean = springConfig.createSingletonBean(ProxyFactoryBean.class)
+                proxiedBean.addProperty("target", new RuntimeBeanReference("_" + beanName))
+                proxiedBean.addProperty("interceptorNames", CollectionUtil.QuickList(beanName + "_advice"))
+                applicationContext.registerBeanDefinition(beanName, proxiedBean.getBeanDefinition())
+            }
+        }
     }
-  }
 
-  public int getOrder() {
-    return 10;
-  }
+    public int getOrder() {
+        return 10
+    }
 }
